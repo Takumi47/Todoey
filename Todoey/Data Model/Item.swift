84140f1a2e17bc307a3793ba287b66d6de78120c
Takumi47 <emtion.tw@gmail.com>
2018-03-20 01:00:00 +0800

    2018-0320: Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by xander on 19/03/2018.
+//  Copyright © 2018 xander. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
